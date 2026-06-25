689637b5e7b3dccf794ef6f02e2bd08cbe7c2b32
dev-in-portfolio <devin.dev.portfolio@gmail.com>
2026-06-25 17:50:45 -0400

    Upgrade PatchSmith to Split Diff Code Viewer & Gutter Girth Mapping

--- src/server.js
+++ src/server.js
@@ -20,6 +20,7 @@ const pool = DATABASE_URL
 
 app.use(cors());
 app.use(express.json({ limit: '2mb' }));
+app.use(express.static('public'));
 app.use((req, _res, next) => {
   const fnPrefix = '/.netlify/functions/server';
   if (req.url === fnPrefix) req.url = '/';
